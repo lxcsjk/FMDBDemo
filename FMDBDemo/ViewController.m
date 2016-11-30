ad13babfaf4610174f8fbbca4d4ba5e58116149f
LXC <yoruolxc@outlook.com>
2016-11-30 19:27:43 +0800

    添加对字典的存取

--- FMDBDemo/ViewController.m
+++ FMDBDemo/ViewController.m
@@ -125,7 +125,7 @@
         FMDatabase *db = [FMDatabase databaseWithPath:fileName];
         if ([db open]){
             //  创建表
-            BOOL result = [db executeUpdate:@"CREATE TABLE 't_down_course' ('id' INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL , 'courseId' INTEGER, 'downPath' VARCHAR(255))"];
+            BOOL result = [db executeUpdate:@"CREATE TABLE 't_down_course' ('id' INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL , 'courseId' INTEGER, 'downPath' VARCHAR(255),'courseInfo' text)"];
             if (result){
                 DLog(@"创建表成功");
                 isTableExist = YES;
@@ -141,11 +141,102 @@
     //    }
 }
 
+/* 字典转JSON字符串 */
+-(NSString*)dictionaryToJson:(NSDictionary *)dic{
+    NSError *parseError = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&parseError];
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
+
+/* 字符串转字典 */
+-(NSMutableDictionary *)dictionaryWithJsonString:(NSString *)jsonString {
+    if (jsonString == nil) {
+        return nil;
+    }
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *err;
+    NSMutableDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                               options:NSJSONReadingMutableContainers
+                                                                 error:&err];
+    if(err) {
+        DLog(@"json解析失败：%@",err);
+        return nil;
+    }
+    return dic;
+}
+
+
 -(void)addData{
     FMDatabase *db = [FMDatabase databaseWithPath:fileName];
+    
+    NSDictionary *dic = @{
+                          @"code": @0,
+                          @"msg": @"success",
+                          @"data": @{
+                                  @"course": @{
+                                          @"id": @24,
+                                          @"crowdId": @4,
+                                          @"ccode": @"KIa8nNpVmc",
+                                          @"cname": @"健健康康",
+                                          @"coverImg": @"http://ocd2lp9uj.bkt.clouddn.com/FaceQ1445612150222.jpg",
+                                          @"description": @"淋漓尽致",
+                                          @"speaker": @2,
+                                          @"speakerName": @"刘欣成",
+                                          @"speakerHeadIcon": @"http://ocd2lp9uj.bkt.clouddn.com/FaceQ1445612150222.jpg",
+                                          @"startTime": @"2016-10-15 11:21:44",
+                                          @"endTime": @"2016-10-15 11:26:50",
+                                          @"liveStatus": @"2",
+                                          @"saveStatus": @"2",
+                                          @"handouts": @[@{
+                                                             @"id": @22,
+                                                             @"convertFlag": @"1",
+                                                             @"createAt": @"2016-09-28 18:21:56",
+                                                             @"updateAt": @"2016-11-23 12:31:32",
+                                                             @"indexurl": @"http://ocd2lp9uj.bkt.clouddn.com/FuCjC3mjt3QrYs7V3lzvicf7zg0B",
+                                                             @"key": @"a.ppt",
+                                                             @"bucket": @"file",
+                                                             @"name": @"a.ppt",
+                                                             @"mimetype": @"application/vnd.ms-powerpoint",
+                                                             @"url": @"http://ocd2lp9uj.bkt.clouddn.com/a.ppt"
+                                                             }, @{
+                                                             @"id": @46,
+                                                             @"convertFlag": @"1",
+                                                             @"createAt": @"2016-09-28 18:21:56",
+                                                             @"updateAt": @"2016-11-23 12:31:32",
+                                                             @"indexurl": @"http://ocd2lp9uj.bkt.clouddn.com/FuCjC3mjt3QrYs7V3lzvicf7zg0B",
+                                                             @"key": @"a.ppt",
+                                                             @"bucket": @"file",
+                                                             @"name": @"a.ppt",
+                                                             @"mimetype": @"application/vnd.ms-powerpoint",
+                                                             @"url": @"http://ocd2lp9uj.bkt.clouddn.com/a.ppt"
+                                                             }],
+                                          @"pics": @[@"http://ocd2lp9uj.bkt.clouddn.com/FpEo7Td-6SmlRxgS1jfRFsstevjc", @"http://ocd2lp9uj.bkt.clouddn.com/FgfMK50FohHcGx4957rOlyz7hDJq", @"http://ocd2lp9uj.bkt.clouddn.com/Fuz7pFQlLuNoxgkCurVMgxRsW3ej", @"http://ocd2lp9uj.bkt.clouddn.com/Fst2-1uX7dh6p4WLdx80Xea8dqLl", @"http://ocd2lp9uj.bkt.clouddn.com/FsEbY5Z-92eZZZJLbKWXkqbjgEuR", @"http://ocd2lp9uj.bkt.clouddn.com/Fj8XHzgdKeAcl5aFcfxJ_wTknM57", @"http://ocd2lp9uj.bkt.clouddn.com/FuCjC3mjt3QrYs7V3lzvicf7zg0B", @"http://ocd2lp9uj.bkt.clouddn.com/FmufT0gIAcUgaDcbO9bSgftrN7q7", @"http://ocd2lp9uj.bkt.clouddn.com/FhFk0-ErPNILDQbaqGG-g-mU8Psy", @"http://ocd2lp9uj.bkt.clouddn.com/FtfpM4g8rd9-Jc-38M1QOlwH4elP", @"http://ocd2lp9uj.bkt.clouddn.com/Fuw2ikIrdc63_E2beeAw0TDqA8Pu", @"http://ocd2lp9uj.bkt.clouddn.com/FheS4Ob_Ve9aVzQwY6p_v195i_mA", @"http://ocd2lp9uj.bkt.clouddn.com/FlZTrqXti3QOw1uKUogcroBjEdQW", @"http://ocd2lp9uj.bkt.clouddn.com/FqQLN3CpgoaTQOMSGvi4DC_pVy7h", @"http://ocd2lp9uj.bkt.clouddn.com/Fn-TCwZCOPJCNv0dH3SmKwjEE9cO"],
+                                          @"playUrl": @{
+                                                  @"saveUrl": @"http://oeaqm5upd.bkt.clouddn.com/KIa8nNpVmc"
+                                                  },
+                                          @"skipPics": @{
+                                                  @"00:00:03": @"http://ocd2lp9uj.bkt.clouddn.com/FpEo7Td-6SmlRxgS1jfRFsstevjc",
+                                                  @"00:00:04": @"http://ocd2lp9uj.bkt.clouddn.com/FgfMK50FohHcGx4957rOlyz7hDJq",
+                                                  @"00:00:05": @"http://ocd2lp9uj.bkt.clouddn.com/Fuz7pFQlLuNoxgkCurVMgxRsW3ej",
+                                                  @"00:00:06": @"http://ocd2lp9uj.bkt.clouddn.com/Fst2-1uX7dh6p4WLdx80Xea8dqLl",
+                                                  @"00:00:07": @"http://ocd2lp9uj.bkt.clouddn.com/FsEbY5Z-92eZZZJLbKWXkqbjgEuR",
+                                                  @"00:00:08": @"http://ocd2lp9uj.bkt.clouddn.com/FpEo7Td-6SmlRxgS1jfRFsstevjc",
+                                                  @"00:00:09": @"http://ocd2lp9uj.bkt.clouddn.com/Fj8XHzgdKeAcl5aFcfxJ_wTknM57",
+                                                  @"00:00:10": @"http://ocd2lp9uj.bkt.clouddn.com/Fuz7pFQlLuNoxgkCurVMgxRsW3ej",
+                                                  @"00:00:11": @"http://ocd2lp9uj.bkt.clouddn.com/FsEbY5Z-92eZZZJLbKWXkqbjgEuR",
+                                                  @"00:00:13": @"http://ocd2lp9uj.bkt.clouddn.com/FsEbY5Z-92eZZZJLbKWXkqbjgEuR",
+                                                  @"00:00:14": @"http://ocd2lp9uj.bkt.clouddn.com/Fuz7pFQlLuNoxgkCurVMgxRsW3ej",
+                                                  @"00:00:15": @"http://ocd2lp9uj.bkt.clouddn.com/FpEo7Td-6SmlRxgS1jfRFsstevjc"
+                                                  }
+                                          }
+                                  }
+                          };
+    NSString *json = [self dictionaryToJson:dic];
+    
     if ([db open]){
-        NSString *insertSql = @"insert into 't_down_course'(courseId,downPath) values(?,?)";
-        BOOL result = [db executeUpdate:insertSql,[NSString stringWithFormat:@"%d",count],fileName];
+        NSString *insertSql = @"insert into 't_down_course'(courseId,downPath,courseInfo) values(?,?,?)";
+        BOOL result = [db executeUpdate:insertSql,[NSString stringWithFormat:@"%d",count],fileName,json];
         if (result){
             DLog(@"添加数据成功");
             [self showAlertWithMessage:@"添加数据 --- 成功" completion:nil];
@@ -169,7 +260,12 @@
             int course = [rs intForColumn:@"id"];
             NSString * courseId = [rs stringForColumn:@"courseId"];
             NSString * downPath = [rs stringForColumn:@"downPath"];
-            DLog(@"course id = %d, courseId = %@, downPath = %@", course, courseId, downPath);
+            NSString * courseInfo = [rs stringForColumn:@"courseInfo"];
+            
+            DLog(@"course id = %d, courseId = %@, downPath = %@  courseInfo = %@", course, courseId, downPath,courseInfo);
+            
+            NSMutableDictionary *dic = [self dictionaryWithJsonString:courseInfo];
+            DLog(@"%@",dic);
         }
         [db close];
     }else{
